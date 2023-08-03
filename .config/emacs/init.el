842a384eeb16fe95aa02d2f8176e9a75d102ae3a
Daniel J Wilcox <danieljwilcox@gmail.com>
2023-08-03 16:01:05 +0100

    emacs google translate

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -19,100 +19,100 @@
 ;; package-selected-packages
   
 (custom-set-variables
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-'(auth-source-save-behavior nil)
-'(connection-local-criteria-alist
-  '(((:application eshell)
-     eshell-connection-default-profile)
-    ((:application tramp :protocol "flatpak")
-     tramp-container-connection-local-default-flatpak-profile)
-    ((:application tramp)
-     tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
-'(connection-local-profile-alist
-  '((eshell-connection-default-profile
-     (eshell-path-env-list))
-    (tramp-container-connection-local-default-flatpak-profile
-     (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
-    (tramp-connection-local-darwin-ps-profile
-     (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
-     (tramp-process-attributes-ps-format
-      (pid . number)
-      (euid . number)
-      (user . string)
-      (egid . number)
-      (comm . 52)
-      (state . 5)
-      (ppid . number)
-      (pgrp . number)
-      (sess . number)
-      (ttname . string)
-      (tpgid . number)
-      (minflt . number)
-      (majflt . number)
-      (time . tramp-ps-time)
-      (pri . number)
-      (nice . number)
-      (vsize . number)
-      (rss . number)
-      (etime . tramp-ps-time)
-      (pcpu . number)
-      (pmem . number)
-      (args)))
-    (tramp-connection-local-busybox-ps-profile
-     (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
-     (tramp-process-attributes-ps-format
-      (pid . number)
-      (user . string)
-      (group . string)
-      (comm . 52)
-      (state . 5)
-      (ppid . number)
-      (pgrp . number)
-      (ttname . string)
-      (time . tramp-ps-time)
-      (nice . number)
-      (etime . tramp-ps-time)
-      (args)))
-    (tramp-connection-local-bsd-ps-profile
-     (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
-     (tramp-process-attributes-ps-format
-      (pid . number)
-      (euid . number)
-      (user . string)
-      (egid . number)
-      (group . string)
-      (comm . 52)
-      (state . string)
-      (ppid . number)
-      (pgrp . number)
-      (sess . number)
-      (ttname . string)
-      (tpgid . number)
-      (minflt . number)
-      (majflt . number)
-      (time . tramp-ps-time)
-      (pri . number)
-      (nice . number)
-      (vsize . number)
-      (rss . number)
-      (etime . number)
-      (pcpu . number)
-      (pmem . number)
-      (args)))
-    (tramp-connection-local-default-shell-profile
-     (shell-file-name . "/bin/sh")
-     (shell-command-switch . "-c"))
-    (tramp-connection-local-default-system-profile
-     (path-separator . ":")
-     (null-device . "/dev/null"))))
-'(custom-safe-themes
-  '("636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "" default))
-'(package-selected-packages
-  '(magit all-the-icons doom-themes ednc elfeed elfeed-org elfeed-tube elfeed-tube-mpv embark embark-consult emmet-mode evil-collection evil-leader evil-surround fd-dired flycheck git-commit git-auto-commit-mode hydra iedit magit-section marginalia mpv ob-async openwith orderless rg s shrink-path undo-tree vertico wgrep which-key yaml-mode))
-'(warning-suppress-types '((comp))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(connection-local-criteria-alist
+   '(((:application eshell)
+      eshell-connection-default-profile)
+     ((:application tramp :protocol "flatpak")
+      tramp-container-connection-local-default-flatpak-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((eshell-connection-default-profile
+      (eshell-path-env-list))
+     (tramp-container-connection-local-default-flatpak-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
+ '(custom-safe-themes
+   '("636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "" default))
+ '(package-selected-packages
+   '(google-translate all-the-icons doom-themes ednc elfeed elfeed-org elfeed-tube elfeed-tube-mpv embark-consult emmet-mode evil-collection evil-leader evil-surround fd-dired flycheck git-auto-commit-mode hydra iedit marginalia mpv ob-async openwith rg s shrink-path undo-tree vertico wgrep which-key yaml-mode))
+ '(warning-suppress-types '((comp))))
 
 ;; require package
 (require 'package)
@@ -635,11 +635,11 @@
 
   
 (custom-set-faces
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-'(org-link ((t (:inherit link :underline nil)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-link ((t (:inherit link :underline nil)))))
 
 (defadvice org-capture
     (after make-full-window-frame activate)
